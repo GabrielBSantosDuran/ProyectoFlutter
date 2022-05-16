512d2916396c6d9e9e54920ae559d6aaa1477bb1
MrFrost14 <gabriel_bodomir@hotmail.com>
2022-05-16 00:16:35 -0400

    Mejoras de Aplicacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mi primera App',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Mi primer App'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-        backgroundColor: Colors.white70,
-      ),
-      backgroundColor: Colors.white54,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:[
-            const Text(
+      title: 'Material App',
+      home: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/Img/back.jpg') ,
+          fit: BoxFit.cover
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            title: const Text('Primera App Flutter'),
+            backgroundColor: Colors.indigo,
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:[
+              const Text(
               'Bienvenido a mi primera app en Flutter',
+                style: TextStyle(color: Colors.white, fontSize: 20),
             ),
             const Text(
               'Elaborado por: Gabriel Santos :)',
+              style: TextStyle(color: Colors.white, fontSize: 20),
             ),
             Container(
-              child: Image.asset('assets/doge.jpg', width: 200,),
+              child: Image.asset('assets/Imagen1.jpg', width: 200,),
               padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
               margin: EdgeInsets.all(20),
               decoration: BoxDecoration(
-                border: Border.all(color: Colors.black26, width: 4),
-                borderRadius: BorderRadius.circular(20)
+                  border: Border.all(color: Colors.black26, width: 4),
+                  borderRadius: BorderRadius.circular(20)
               ),
             ),
-            const Text(
-              'Toca el botón para que el perrito empiece a incrementar los números',
+                const Text(
+                  'Actualmente estudio de Ingenieria de Sistemas',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+
+
+
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
